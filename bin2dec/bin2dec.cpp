e478454e4955d6c7edf2482c6bc740ab5e03fe74
re1ota <alessandro.relota@hotmail.com>
2026-04-16 01:47:38 +0200

    Initial Commit: added Bin2Dec project

---
+++ bin2dec/bin2dec.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+int main() {
+    std::string number;
+    std::cout << "Please enter your binary number: ";
+    std::getline(std::cin, number);
+
+    if (number.empty()) {
+        throw std::runtime_error("Wrong input! Please enter a binary number");
+    }
+
+    int result = 0;
+    const int uncutLength = number.length();
+    int position = 0;
+    int length = 0;
+
+    for (int i = 0; i < uncutLength; i++) {
+        if (number.at(i) == '1' || number.at(i) == '0') {
+            length++;
+        }
+    }
+
+    if (length > 8 || length == 0) {
+        throw std::runtime_error("Wrong input! Please enter a binary number with a maximum length of 8!");
+    }
+
+    for (int i = 0 ; i < uncutLength; i++) {
+        char c = number.at(i);
+        if (c == '1') {
+            result += (1 << (length - 1 - position));
+            position++;
+        }else if (c == '0') {
+            position++;
+        }else if (c == ' ') {
+            continue;
+        }else {
+            throw std::runtime_error("Wrong input! Please enter a binary number");
+        }
+    }
+
+    std::cout << result << std::endl;
+    return 0;
+}
